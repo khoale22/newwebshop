5805b6073d36445be2d852b8de63c2867b758122
Your Name <hbkkhoa1@gmail.com>
2018-09-16 23:50:06 +0700

    fds

--- BookAppNew/src/app/bookAdd-function/bookAdd.component.js
+++ BookAppNew/src/app/bookAdd-function/bookAdd.component.js
@@ -13,6 +13,7 @@ component('bookAdd' , {
         });
 
         seft.addBook = function(id, title, topic, author, cost) {
+            alert( "fsdf" + id);
             seft.books.push({
                 "bookId": id,
                 "bookTitle": title,
@@ -20,6 +21,23 @@ component('bookAdd' , {
                 "author": author,
                 "cost": cost,
                 "imgUrl": "imgs/DefaultBookImage.jpg"
+            });
+       ///// test
+            var dataObj = {
+                 bookId: id,
+                bookTitle: title,
+                topic: topic,
+                author: author,
+                cost: cost,
+                imgUrl : "imgs/DefaultBookImage.jpg"
+            };
+            var res = $http.post('data/phones.json', dataObj);
+            res.success(function(data, status, headers, config) {
+                $scope.message = data;
+            });
+            res.error(function(data, status, headers, config) {
+                alert( "Can not push data to json file");
+       ///// test
             });
             $location.path('/home/admin');
         }
