9821e0650652cad5c01f3ec0eef1d0205aa436f9
Your Name <hbkkhoa1@gmail.com>
2019-03-07 21:52:46 +0700

    ewrfdsf

---
+++ AGLinternet/WebContent/componentWithEmit/app.js
@@ -0,0 +1,56 @@
+﻿
+    var app = angular.module('MainApp', [])
+
+        .component('parentComponent', {
+
+            controller : ["$scope",function($scope) {
+              var $ctrl = this ;
+                $ctrl.ten = "khoa";
+                $ctrl.OnClick = function (evt) {
+                    $scope.$broadcast("SendDown", $ctrl.ten );
+                }
+                $scope.$on("SendDown", function (evt, data) {
+                    $ctrl.Message = "Inside SendDown handler of MyController1 : " + data;
+                });
+                $scope.$on("SendDown", function (evt, data) {
+                    $ctrl.Message = "Inside SendDown handler of MyController1 : " + data;
+                });
+
+                //handle SendUp event
+                $scope.$on("SendUp", function (evt, data) {
+                    $ctrl.Message = "Inside SendUp handler of MyController1 : " + data;
+                });
+            }],
+                template : ' <h2>Iam parentComponent</h2>'+
+            '<p> userName : {{$ctrl.Message }}</p>'+
+                    '<p><button ng-click="$ctrl.addName = $ctrl.OnClick($event)">Broardcast</button></p>'+
+            '<child-component></child-component>'
+        })
+        .component('childComponent', {
+            controller : ["$scope",function($scope) {
+                var $ctrl = this ;
+
+                //handle SendDown event
+                $scope.$on("SendDown", function (evt, data) {
+                    $ctrl.Message = "Inside SendDown handler of MyController3 : " + data;
+                });
+
+                //emit SendUp event up
+                $ctrl.OnClick = function (evt, data) {
+                    $scope.$emit("SendUp", "some data");
+                }
+
+                //handle SendUp event
+                $scope.$on("SendUp", function (evt, data) {
+                    $ctrl.Message = "Inside SendUp handler of MyController3 : " + data;
+                });
+            }],
+            template : ' <h2>Iam childComponent</h2>'+
+               ' <h2>{{$ctrl.addName}}</h2>'+
+                '<p> userName : {{$ctrl.Message }}</p>'+
+                '<p><button ng-click="$ctrl.addName = $ctrl.OnClick($event)">Emit</button></p>'
+
+        })
+    ;
+
+
---
+++ AGLinternet/WebContent/session1/app.js
@@ -0,0 +1,34 @@
+﻿
+    var app = angular.module('MainApp', [])
+
+        .component('outerComp', {
+          bindings :{
+              //userName : '=',
+             // addName : '='
+          },
+            controller : [function () {
+              var $ctrl = this ; //// SU DUNG var self tuong tu ,va ở template vẫn là  {{$ctrl.userName }}
+               $ctrl.userName = "khoaLe1";
+
+            }],
+                template : ' <h2>Iam outerComp</h2>'+
+            '<p> userName : {{$ctrl.userName }}</p>'+
+            '<inner-comp add-name="$ctrl.userName"></inner-comp>'
+        })
+        .component('innerComp', {
+            bindings :{
+                addName : '='
+            },
+            controller : [function () {
+                var $ctrl = this ;
+                $ctrl.ten = "ngatran";
+            }],
+            template : ' <h2>Iam innercomp444</h2>'+
+               ' <h2>{{$ctrl.addName}}</h2>'+
+               // '<p>New Name : <input ng-model="$ctrl.newName"/></p>'+
+                '<p><button ng-click="$ctrl.addName = $ctrl.ten">ADDNAME</button></p>'
+
+        })
+    ;
+
+
